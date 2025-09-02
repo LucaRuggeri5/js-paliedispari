ab01d637ba89c7ce955ec71f6720c535b27cce4e
LucaRuggeri5 <luca.ruggeri5@gmail.com>
2025-09-02 16:44:43 +0200

    finalizzato esercizio palindromi

--- Palindroma/script.js
+++ Palindroma/script.js
@@ -1,3 +1,17 @@
 // Palidroma
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
+
+
+const parolaNormale = prompt("Inserisci una parola per verificare se è palindroma:");
+
+function Palindroma(parola) {
+  const parolaInvertita = parola.split("").reverse().join("");
+  return parola === parolaInvertita;
+}
+
+if (Palindroma(parolaNormale)) {
+  console.log("è palindroma!");
+} else {
+  console.log("non è palindroma!");
+}
